4efc39904a0d66959842317393d8f72e66830347
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2023-12-22 23:47:33 +0200

    done

--- factors.py
+++ factors.py
@@ -1,32 +1,29 @@
-#!/usr/bin/python3
-# library to get arguments
 import sys
+import math
 
+def factorize(n):
+    # Check if the number is even
+    if n % 2 == 0:
+        return 2, n // 2
 
-# fn unpack number factorial
-def fc():
-    """
-    function fc to search file to convert number and format n=p*q
-    """
-    try:
-        revfile = sys.argv[1]
-        with open(revfile) as f:
-            for revnumber in f:
-                revnumber = int(revnumber)
-                if revnumber % 2 == 0:
-                    print("{}={}*{}".format(revnumber, revnumber // 2, 2))
-                    continue
-                i = 3
-                while i < revnumber // 2:
-                    if revnumber % i == 0:
-                        print("{}={}*{}".format(revnumber, revnumber // i, i))
-                        break
-                    i = i + 2
-                if i == (revnumber // 2) + 1:
-                    print("{}={}*{}".format(revnumber, revnumber, 1))
-    except (IndexError):
-        pass
+    # If the number is not even, start checking odd numbers
+    for i in range(3, math.isqrt(n) + 1, 2):
+        if n % i == 0:
+            return i, n // i
 
+    # If no factor found, the number is a prime
+    return n, 1
 
-# autostart
-fc()
+def main():
+    # Open the file
+    with open(sys.argv[1], 'r') as file:
+        for line in file:
+            # Read each line (number)
+            n = int(line.strip())
+            # Factorize the number
+            p, q = factorize(n)
+            # Print the result
+            print(f"{n}={p}*{q}")
+
+if __name__ == "__main__":
+    main()
